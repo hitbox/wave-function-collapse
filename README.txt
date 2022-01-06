3f212f725d322f51b9dbc7218f17a4b2c861b0e4
Carl Harris <elgoogemail2007@gmail.com>
2022-01-06 05:50:10 -0500

    init

---
+++ README.txt
@@ -0,0 +1,6 @@
+# Wave Function Collapse
+
+Trying to wrap my head around wave function collapse with Python.
+
+https://www.gridbugs.org/wave-function-collapse/
+
---
+++ wave_function_collapse.py
@@ -0,0 +1,108 @@
+import argparse
+import contextlib
+import os
+
+from collections import Counter
+from itertools import product
+
+with contextlib.redirect_stdout(open(os.devnull,'w')):
+    import pygame
+
+def get_tiles(input_image, size):
+    output = []
+    input_width, input_height = input_image.get_size()
+    for y, x in product(range(input_height), range(input_width)):
+        surface = pygame.Surface((size,)*2)
+        for offx, offy in product(range(size), repeat=2):
+            source = ((x+offx) % input_width, (y+offy) % input_height)
+            color = input_image.get_at(source)
+            surface.set_at((offx, offy), color)
+        output.append(surface)
+    return output
+
+
+def get_surface_table(surf):
+    width, height = surf.get_size()
+    table = tuple(
+        tuple(
+            tuple(surf.get_at((x,y))) for x in range(width)
+        )
+        for y in range(height)
+    )
+    return table
+
+def sample_input_image(size, color, background):
+    # the first 4x4 input image generator
+    # https://www.gridbugs.org/wave-function-collapse/
+    rect = pygame.Rect((0,0), size)
+    surface = pygame.Surface(size, flags=pygame.SRCALPHA)
+    surface.fill(background)
+    pygame.draw.line(surface, color, (1,rect.top), (1,rect.bottom), 1)
+    pygame.draw.line(surface, color, (rect.left,rect.bottom-2), (rect.right,rect.bottom-2), 1)
+    return surface
+
+def post_quit():
+    pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+def main(argv=None):
+    """
+    Wave function collapse in pygame
+    """
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args(argv)
+
+    pygame.display.init()
+    screen = pygame.display.set_mode((800,800))
+    buffer = pygame.Surface((100,100))
+    frame = buffer.get_rect()
+    clock = pygame.time.Clock()
+
+    sample = sample_input_image((4,8), (200,)*3, (0,)*3)
+    output = get_tiles(sample, 3)
+
+    # index aligned
+    table_surface_map = dict((get_surface_table(surf), surf) for surf in output)
+    # inserting keys into the dict will have made the values unique.
+    unique_tiles = list(table_surface_map.values())
+
+    # NOTE
+    # 2022-01-05:
+    #   Left off here. The tiles of the input image are "rules" for what pixels
+    #   may be drawn in the output image. How the rules are applied is not well
+    #   understood. Since pygame Surface objects aren't hashable mapping and
+    #   counting and finding unique tiles is difficult.
+
+    # count dupes
+    counter = Counter(table_surface_map.keys())
+
+    # display input image
+    buffer.fill((40,)*3)
+    rect = sample.get_rect(center=frame.center)
+    buffer.blit(sample, rect)
+
+    # display tiles
+    print(f'{len(output)=}')
+    x = y = 2
+    for surf in output:
+        buffer.blit(surf, (x, y))
+        if surf.get_rect(x=x,y=y).right > frame.centerx // 4:
+            y += surf.get_height() + 2
+            x = 2
+        else:
+            x += surf.get_width() + 2
+
+    pygame.transform.scale(buffer, (800,)*2, screen)
+
+    running = True
+    while running:
+        elapsed = clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_ESCAPE, pygame.K_q):
+                    post_quit()
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
